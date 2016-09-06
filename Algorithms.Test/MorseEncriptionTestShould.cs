e6f46ffc6314e41140792ae190b3af7f2afb0034
Gabriel <Gabriel@GABRIEL-PC>
2016-09-06 17:47:07 +0200

    Add text encription to morse

---
+++ Algorithms.Test/MorseEncriptionTestShould.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Algorithms.Test
+{
+    [TestClass]
+    public class MorseEncriptionTestShould
+    {
+        [TestMethod]
+        public void EncriptTextToMorse()
+        {
+            MorseAlgorithm algorithm = new MorseAlgorithm();
+
+            string morse = algorithm.EncriptText("hello world");
+
+            morse.Should().Be(".... . .-.. .-.. --- / .-- --- .-. .-.. -..");
+        }
+    }
+}
